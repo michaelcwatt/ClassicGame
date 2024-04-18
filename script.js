2454991bb3bad66b8a8d76aa15854d3eaf8f05d1
michael watt <michaelcolinwatt@icloud.comgit config --global user.name michael>
2024-04-18 18:07:04 +0100

    Classic game - 2 - 18.4.24 - added first few lines of js. - created an array and got the computer to pick a random element.

---
+++ script.js
@@ -0,0 +1,9 @@
+function getComputerChoice (){
+const choice = ["Rock", "Paper", "Scissor"]
+const random = math.floor(math.random()*choice.length)
+console.log(random)
+}
+
+// create an array with rock paper scissors //  DONE
+// make the computer select one of the three at random// var randomNumber = Math.floor(Math.random()*textArray.length);
+// print randomly selected thing //
